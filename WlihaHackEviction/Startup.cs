dc210ecb6bac163cbcc156425ed338899acf711e
NTDEV\catalda <catalda@microsoft.com>
2019-07-23 16:43:48 -0700

    add swagger

--- WlihaHackEviction/Startup.cs
+++ WlihaHackEviction/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WlihaHackEviction.Models;
+using Microsoft.OpenApi.Models;
 
 namespace WlihaHackEviction
 {
@@ -32,6 +33,11 @@ namespace WlihaHackEviction
                 options.UseSqlServer(Configuration.GetConnectionString("SQLConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,9 @@ namespace WlihaHackEviction
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 
             app.UseHttpsRedirection();
             app.UseMvc();
