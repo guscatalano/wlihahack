eda59eb75f430e1ae5aaf4a24f2c9d791b7e8f7a
snehashankar <snehashankar.249@gmail.com>
2019-07-22 18:44:52 -0700

    Adding DB Connection

--- WlihaHackEviction/Models/Model.cs
+++ WlihaHackEviction/Models/Model.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace WlihaHackEviction.Models
 {
+    [DataContractAttribute(Name ="TenantsInfo")]
     public class TenantInfo
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public int NumberOfPpl { get; set; }
-        public long AddressId { get; set; }
+        public int AddressId { get; set; }
     }
 
     public class AddressInfo
