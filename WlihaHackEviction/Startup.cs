8f591f6ce4fc2c141915e7cd401e2ee59195ae68
NTDEV\catalda <catalda@microsoft.com>
2019-07-23 18:06:33 -0700

    make it work with keyvault and add first view

--- WlihaHackEviction/Startup.cs
+++ WlihaHackEviction/Startup.cs
@@ -28,9 +28,11 @@ namespace WlihaHackEviction
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionStringFromConfig = Configuration.GetValue<string>("MySQLConnection");
+
             // Configure the DBContext
             services.AddDbContext<EvictionDatabaseContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("SQLConnection")));
+                options.UseSqlServer(connectionStringFromConfig));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
