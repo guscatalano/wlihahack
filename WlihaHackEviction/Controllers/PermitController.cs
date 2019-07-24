c7b685853d57b1be01aa179f144db6e046b1d7c3
Megan Rogge <t-merogg@microsoft.com>
2019-07-24 10:25:45 -0700

    add permit controller

--- WlihaHackEviction/Controllers/PermitController.cs
+++ WlihaHackEviction/Controllers/PermitController.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
+using Serenity.Data;
+using WlihaHackEviction.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,45 +13,63 @@ namespace WlihaHackPermit.Controllers
     [Route("api/[controller]")]
     public class PermitController : Controller
     {
-        private PermitDatabaseContext _dbContext;
-
-        public PermitController(PermitDatabaseContext context)
+        public PermitController()
         {
-            _dbContext = context;
         }
 
         // GET: api/Permit
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<TenantInfo> GetTenantsEvictedWithoutPermit()
         {
-            return new string[] { "value1", "value2" };
+            string query = @"SELECT *
+            FROM [dbo].[TenantsInfo]
+            WHERE [dbo].[TenantsInfo].[Id] =
+            (SELECT [dbo].[EvictionInfo].[TenantId]
+            FROM [dbo].[EvictionInfo]
+            WHERE 
+	        NOT EXISTS(
+	        SELECT* 
+	        FROM [dbo].[AddressInfo]
+	        LEFT JOIN [dbo].[PermitInfo]
+	        ON [dbo].[AddressInfo].[StreetAddress] like [dbo].[PermitInfo].[OriginalAddress1]))";
+
+            SqlConnection connection = new SqlConnection("connectionString");
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<TenantInfo> tenantInfo = new List<TenantInfo>();
+
+            while (reader.Read())
+            {
+                TenantInfo t = new TenantInfo();
+                t.Name = reader["Name"].ToString();
+                t.Email = reader["Email"].ToString();
+                t.Id = (int)reader["Id"];
+                t.NumberOfPpl = (int)reader["NumberOfPpl"];
+                t.Phone = reader["Phone"].ToString();
+                tenantInfo.Add(t);
+            }
+            return tenantInfo;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetTenantsEvictedWithExpiredPermit()
+        {
+            throw new NotImplementedException();
         }
 
         // GET api/Permit/Id
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{ id}")]
+        public string Get(int tenantId)
         {
             // Get PermitInfo
-            return "Hello World";
+            return "not implemented yet";
         }
 
-        // POST api/Permit
-        [HttpPost]
-        public void Post([FromBody]string value)
+        // GET api/Permit/Refresh
+        [HttpGet]
+        public void Get([FromBody]string value)
         {
-
-        }
-
-        // PUT api/Permit/Id
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/Permit/Id
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            // refreshes the permit info table by downloading the csv and importing it to the table
         }
     }
 }
