d2212789b46a7da9105d491a81fc7dd5a2107c90
Megan Rogge <t-merogg@microsoft.com>
2019-07-24 10:30:01 -0700

    undo some changes

--- WlihaHackEviction/Models/Model.cs
+++ WlihaHackEviction/Models/Model.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WlihaHackEviction.Models
 {
@@ -22,11 +18,6 @@ namespace WlihaHackEviction.Models
         public int NumberOfPpl { get; set; }
         [DataMember]
         public int AddressId { get; set; }
-
-        public static implicit operator TenantInfo(ActionResult<TenantInfo> v)
-        {
-            throw new NotImplementedException();
-        }
     }
 
     [DataContract]
@@ -57,5 +48,35 @@ namespace WlihaHackEviction.Models
         public int Id { get; set; }
         [DataMember]
         public DateTime DateOfEviction { get; set; }
+    }
+
+    [DataContract]
+    public class PermitInfo
+    {
+        [DataMember]
+       public string PermitNum { get; set; }
+       public string PermitClass { get; set; }
+       public string PermitClassMapped { get; set; }
+       public string PermitTypeMapped { get; set; }
+       public string PermitTypeDesc{ get; set; }
+       public string Description { get; set; }
+       public int HousingUnits{ get; set; }
+       public int HousingUnitsRemoved { get; set; }
+       public int HousingUnitsAdded { get; set; }
+       public float EstProjectCost { get; set; }
+       public DateTime AppliedDate { get; set; }
+       public DateTime IssuedDate { get; set; }
+       public DateTime ExpiresDate { get; set; }
+       public DateTime CompletedDate { get; set; }
+       public string StatusCurrent { get; set; }
+       public string OriginalAddress1 { get; set; }
+       public string OriginalCity { get; set; }
+       public string OriginalState { get; set; }
+       public int OriginalZip { get; set; }
+       public string ContractorCompanyName { get; set; }
+       public string Link { get; set; }
+       public float Latitude { get; set; }
+       public float Longitude { get; set; }
+       public string Location1 { get; set; }
     }
 }
